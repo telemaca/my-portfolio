f82c1e570ec6185cfceba4597701b2657ee8f65a
Florencia Holzmann <florholzmann@gmail.com>
2025-07-28 14:18:26 -0300

    Adjusting main sidebar styles

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -21,119 +21,138 @@ const Sidebar = ({ onOpenWindow }: Props) => {
         <div className={styles.profile}>
           <Avatar />
           <div>
-            <div className={styles.username}>Flor_Holzmann</div>
-            {/* <div className={styles.status}>🟢 En línea</div> */}
+            <div className={styles.username}>
+              Flor Holzmann <span>(Online)</span>
+            </div>
+            <div className={styles.status}>Developer</div>
           </div>
         </div>
       </div>
 
-      <div className={styles.customDivider}></div>
-
       <div className={styles.menu}>
-        <h3>Info personal</h3>
-        <ul>
-          <li onClick={() => onOpenWindow("about")}>
+        <div className={styles.lateral}>
+          <div className={styles.logoContainer}>
             <Image
               src="/msn-online.png"
               alt="Msn logo"
-              width={32}
-              height={37}
+              width={45}
+              height={50}
               className={styles.chatIcon}
             />
-            <div>Sobre mí</div>
+          </div>
+        </div>
+        <ul className={styles.contactsList}>
+          <li className={styles.listSection}>
+            <h4>Info personal ( 3 )</h4>
+            <ul>
+              <li onClick={() => onOpenWindow("about")}>
+                <Image
+                  src="/msn-online.png"
+                  alt="Msn logo"
+                  width={30}
+                  height={35}
+                  className={styles.chatIcon}
+                />
+                <div>Sobre mí</div>
+              </li>
+              <li onClick={() => onOpenWindow("stack")}>
+                {" "}
+                <Image
+                  src="/msn-online.png"
+                  alt="Msn logo"
+                  width={30}
+                  height={35}
+                  className={styles.chatIcon}
+                />
+                <div>Stack técnico</div>
+              </li>
+              <li onClick={() => onOpenWindow("work")}>
+                {" "}
+                <Image
+                  src="/msn-online.png"
+                  alt="Msn logo"
+                  width={30}
+                  height={35}
+                  className={styles.chatIcon}
+                />
+                <div>Experiencia laboral</div>
+              </li>
+            </ul>
           </li>
-          <li onClick={() => onOpenWindow("stack")}>
-            {" "}
-            <Image
-              src="/msn-online.png"
-              alt="Msn logo"
-              width={32}
-              height={37}
-              className={styles.chatIcon}
-            />
-            <div>Stack técnico</div>
+          <li className={styles.listSection}>
+            <h4>Mis proyectos ( 5 )</h4>
+            <ul>
+              <li onClick={() => onOpenWindow("about")}>
+                <Image
+                  src="/idle.png"
+                  alt="Msn logo"
+                  width={22}
+                  height={27}
+                  className={styles.chatIcon}
+                />
+                <div>Juegos de palabras app</div>
+              </li>
+              <li onClick={() => onOpenWindow("stack")}>
+                {" "}
+                <Image
+                  src="/idle.png"
+                  alt="Msn logo"
+                  width={22}
+                  height={27}
+                  className={styles.chatIcon}
+                />
+                <div>Etimología diaria</div>
+              </li>
+              <li onClick={() => onOpenWindow("projects")}>
+                {" "}
+                <Image
+                  src="/idle.png"
+                  alt="Msn logo"
+                  width={22}
+                  height={27}
+                  className={styles.chatIcon}
+                />
+                <div>Pasapalabra!</div>
+              </li>
+              <li onClick={() => onOpenWindow("contact")}>
+                {" "}
+                <Image
+                  src="/idle.png"
+                  alt="Msn logo"
+                  width={22}
+                  height={27}
+                  className={styles.chatIcon}
+                />
+                <div>My EnglishBuddy</div>
+              </li>
+              <li onClick={() => onOpenWindow("contact")}>
+                {" "}
+                <Image
+                  src="/idle.png"
+                  alt="Msn logo"
+                  width={22}
+                  height={27}
+                  className={styles.chatIcon}
+                />
+                <div>Rock, Paper, Scissors</div>
+              </li>
+            </ul>
           </li>
-          <li onClick={() => onOpenWindow("work")}>
-            {" "}
-            <Image
-              src="/msn-online.png"
-              alt="Msn logo"
-              width={32}
-              height={37}
-              className={styles.chatIcon}
-            />
-            <div>Experiencia laboral</div>
-          </li>
-        </ul>
-        <h3>Mis proyectos</h3>
-        <ul>
-          <li onClick={() => onOpenWindow("about")}>
-            <Image
-              src="/idle.png"
-              alt="Msn logo"
-              width={32}
-              height={37}
-              className={styles.chatIcon}
-            />
-            <div>Juegos de palabras app</div>
-          </li>
-          <li onClick={() => onOpenWindow("stack")}>
-            {" "}
-            <Image
-              src="/idle.png"
-              alt="Msn logo"
-              width={32}
-              height={37}
-              className={styles.chatIcon}
-            />
-            <div>Etimología diaria</div>
-          </li>
-          <li onClick={() => onOpenWindow("projects")}>
-            {" "}
-            <Image
-              src="/idle.png"
-              alt="Msn logo"
-              width={32}
-              height={37}
-              className={styles.chatIcon}
-            />
-            <div>Pasapalabra!</div>
-          </li>
-          <li onClick={() => onOpenWindow("contact")}>
-            {" "}
-            <Image
-              src="/idle.png"
-              alt="Msn logo"
-              width={32}
-              height={37}
-              className={styles.chatIcon}
-            />
-            <div>My EnglishBuddy</div>
-          </li>
-          <li onClick={() => onOpenWindow("contact")}>
-            {" "}
-            <Image
-              src="/idle.png"
-              alt="Msn logo"
-              width={32}
-              height={37}
-              className={styles.chatIcon}
-            />
-            <div>Rock, Paper, Scissors</div>
-          </li>
-        </ul>
-        <h3>Contacto</h3>
-        <ul>
-          <li onClick={() => onOpenWindow("contact")}>
-            {" "}
-            <Image
-              src="/offline.png"
-              alt="Msn logo"
-              width={32}
-              height={37}
-              className={styles.chatIcon}
-            />
-            <div>Escribime!</div>
+          <li className={styles.listSection}>
+            <h4>Contacto ( 1 )</h4>
+            <ul>
+              <li onClick={() => onOpenWindow("contact")}>
+                {" "}
+                <Image
+                  src="/offline.png"
+                  alt="Msn logo"
+                  width={22}
+                  height={27}
+                  className={styles.chatIcon}
+                />
+                <div>Escribime!</div>
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
